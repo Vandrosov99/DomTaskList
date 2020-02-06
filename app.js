495e69a1443bd733bc2905f172d422c9666f7a42
rpk63 <Vandrosov99>
2020-02-06 16:39:33 +0200

    add 3 types of clrBtn + filter

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const form = document.querySelector('form') // верхняя форма
 const tasklist = document.querySelector('.collection') // ul куда будем вставлять li + a
-const clearBtn = document.getElementsByClassName('clear-tasks')
+const clearBtn = document.querySelector('.clear-tasks')
 // кнопка удаление таска
 const filter = document.querySelector('#filter')
 // ввод фильтра
@@ -26,7 +26,7 @@ function loadEventListeners() {
         //create text node and append
 
         li.appendChild(document.createTextNode(taskInput.value))
-        console.log(taskInput.value)
+        // console.log(taskInput.value)
         //create krestik link
         const link = document.createElement('a');
         //add classname
@@ -37,16 +37,59 @@ function loadEventListeners() {
         //append li to ul
         tasklist.appendChild(li)
 
-        console.log(tasklist)
         taskInput.value = '';
 
         e.preventDefault();
+    });
+
+    tasklist.addEventListener('click', function (e) {
+
+
+        if (e.target.parentElement.classList.contains('delete-item')) {
+
+            e.target.parentElement.parentElement.remove();
+
+        }
+
+    });
+    // удаление ттпа очередь первого добавил первого и удалил
+    // clearBtn.addEventListener('click', function () {
+    //     console.log(tasklist.children.length)
+    //     for (let i = 0; i <= tasklist.children.length; i++) {
+    //         console.log(tasklist.children[i])
+    //         tasklist.children[i].remove();
+    //         break;
+    //     }
+    // })
+
+    // удаление типа стек первого добавил последним удалил
+    // clearBtn.addEventListener('click', function () {
+    //     console.log(tasklist.children.length)
+    //     for (let i = tasklist.children.length - 1; i >= 0; i--) {
+    //         console.log(tasklist.children[i])
+    //         tasklist.children[i].remove();
+    //         break;
+    //     }
+    // })
+    // удаление всех
+    clearBtn.addEventListener('click', function () {
+        while (tasklist.firstChild) {
+            tasklist.firstChild.remove()
+        }
     })
 
+    filter.addEventListener('keyup', function (e) {
 
+        const text = e.target.value.toLowerCase();
+        document.querySelectorAll('.collection-item').forEach(function (task) {
 
+            const item = task.firstChild.textContent;
+            if (item.toLowerCase().indexOf(text) != -1) {
+                task.style.display = 'block';
 
-
-
-
+            } else {
+                task.style.display = 'none';
+            }
+        })
+    })
 }
